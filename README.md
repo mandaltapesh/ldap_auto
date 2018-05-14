07fe3bb7147401e5a06284c5bfcca9de0c7884d6
mandaltapesh <tapesh.mandal@hotmail.com>
2018-05-14 17:42:17 +0530

    Initial commit for this repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This script automates installation of OpenLDAP on Ubuntu and creates two users from the ldif files.
---
+++ ldap.py
@@ -0,0 +1,18 @@
+import subprocess
+
+#responses = ["\n", "redhat.com\n", "redhat\n", "secret", "secret", "\n", "\n", "\n", "\n"]
+
+proc_pkg_install = subprocess.Popen(["sudo", "apt-get", "-y", "install", "slapd", "ldap-utils"], stdin=subprocess.PIPE)
+#proc_pkg_install.stdin.write(passw+"\n")
+proc_pkg_install.communicate()
+
+proc_pkg_install.stdin.close()
+
+slapd_conf = subprocess.call(["sudo", "dpkg-reconfigure", "--default-priority", "slapd"])
+
+cmd1 = ["ldapadd", "-D", "cn=admin,dc=redhat,dc=com", "-w", "secret", "-f", "org.ldif"]
+org_create = subprocess.call(cmd1)
+
+cmd2 = ["ldapadd", "-D", "cn=admin,dc=redhat,dc=com", "-w", "secret", "-f", "users.ldif"]
+add_users = subprocess.call(cmd2)
+ 
